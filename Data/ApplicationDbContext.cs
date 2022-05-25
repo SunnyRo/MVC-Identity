eb37bde7fdf64cfd7866b55ba321d8428857f843
sun <alex.vo.1993@gmail.com>
2022-05-25 06:31:27 -0700

    origin

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PlayerWeb.Models;
+
+namespace PlayerWeb.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Player> Players { get; set; }
+}
